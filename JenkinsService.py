a30280fe891798716ce206bb704de7e1d7f1f0c8
Sandeep <patelsandeep88@gmail.com>
2022-11-10 13:05:27 +0530

    add python file

---
+++ JenkinsService.py
@@ -0,0 +1,22 @@
+
+import jenkins
+
+j = jenkins.Jenkins('http://192.168.56.1:8080', username='admin', password='root')
+
+# print the user and jenking version
+user = j.get_whoami()
+version = j.get_version()
+#print('Hello %s from Jenkins %s' % (user['fullName'], version))
+
+
+# print the jobs count number 
+Total_Count = j.jobs_count()
+
+def JenkinsInfo():
+    print('*'*80)
+    print('*'*24,'Print the Jenkins Information','*'*24)
+    print('*'*80)
+    print('[1].Jenkins User Name ===>',user['fullName'])
+    print('[2].Jenkins version ===>',version)
+    print('[3].Total Number of the Jenkins job Running ===>',Total_Count)
+    print('*'*80)
---
+++ Main.py
@@ -0,0 +1,44 @@
+# this file is main file which is connted to all the file
+#
+# import all the library 
+import os
+from time import sleep
+import JenkinsService as JS
+# os.system('ls')
+
+# main code started 
+###################################################################################
+while True:
+    print()
+    print('*'*80)
+    print('*'*24,'Welcome to Devops Menu Program','*'*24)
+    print('*'*80)
+    print('''
+        [1] Press A ---> Jenkins Service
+        [2] Press B ---> AWS Cloud Service
+        [3] Press C ---> Ansible Service 
+        If you Want Exit Press'(E/e)'
+        ''')
+    print('*'*80)
+    UserInput = input('Choose Your option: ')
+    if UserInput == 'A' or UserInput == 'a':
+        JS.JenkinsInfo()
+        os.system('clear')
+    elif UserInput == 'B' or UserInput == 'b':
+        print('b')
+        os.system('clear')
+    elif UserInput == 'C' or UserInput == 'c':
+        print('c')
+        os.system('clear')
+    elif UserInput == 'D' or UserInput == 'd':
+        print('d')    
+    elif UserInput == 'E' or UserInput == 'e':
+        print('*'*80)
+        print('*'*21,'Thank You For Using Devops Program..','*'*21)
+        print('*'*80)
+        exit()   
+    else:
+        print('woring')
+        os.system('clear')
+
+
